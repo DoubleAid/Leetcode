a58bb30fcf48c4bac2988f87875d2c98ddcf179a
guanggang in home wsl <guanggang tianxia wudi>
2024-05-22 02:26:28 +0800

    add current task

---
+++ 38.count-and-say.cpp
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=38 lang=cpp
+ *
+ * [38] Count and Say
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string countAndSay(int n) {
+        string result = "1"
+        int count = 1;
+        while (count < n) {
+
+        }
+    }
+
+    string generateNewString(string s) {
+        string result;
+        int index = 0;
+        while (index < s.size()) {
+            
+        }
+    }
+};
+// @lc code=end
+
